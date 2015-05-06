d2cc5edfe06ddd37b1b6e7aeb4cca6d2af2696e9
lyfeyaj <lyfeyaj@gmail.com>
2015-05-06 17:16:59 +0800

    fix bugs

--- examples/rest.js
+++ examples/rest.js
@@ -7,20 +7,23 @@
 
 var express = require('express');
 var util = require('util');
-var poplar = require('./poplar');
+var poplar = require('./../lib/poplar');
 
 var app = express();
-var api = poplar();
+var api = poplar.create();
 
 app.disable('x-powered-by');
 
-var Entity = require('../../lib/entity');
-var ApiBuilder = requrie('../../lib/api_builder');
+var Entity = require('./../lib/entity');
+var ApiBuilder = require('./../lib/api_builder');
 
+// 用户接口数据返回
 var UserEntity = new Entity({
   username: true,
   age: true,
   description: { as: 'selfIntroduction' },
+  nation: { value: 'China' },
+  gender: { default: 'unknown' },
   fullname: function(obj) {
     return util.format('%s %s', obj.firstName || '', obj.lastName || '');
   },
@@ -29,21 +32,20 @@ var UserEntity = new Entity({
   },
   hasCreditCard: function(obj) {
     return obj.creditCard ? true : false;
-  },
-  nation: { value: 'China' },
-  gender: { default: 'unknown' }
+  }
 });
 
-var UserApi = new ApiBuilder({
+var UserApi = new ApiBuilder('users', {
   basicPath: '/u'
 });
 
 UserApi.define('info', {
   accepts: [
-    { arg: 'id', type: 'integer', validates: { require: true }, description: 'Get Username' }
+    { arg: 'id', type: 'integer', validates: { require: { message: 'id 不能为空' } }, description: 'Get Username' }
   ],
   http: { path: 'info', verb: 'get' },
-  returns: UserEntity
+  presenter: UserEntity,
+  returns: 'raw'
 }, function(params, cb) {
   return {
     username: 'Felix Liu',
@@ -56,9 +58,16 @@ UserApi.define('info', {
   };
 });
 
-api.define('users', UserApi);
+UserApi.before('*', function(ctx, next) {
+  console.log('before.users.* called');
+  next();
+});
 
-app.use(api.handler('rest'));
-app.use(express.static('public'));
+api.use(UserApi);
 
-app.listen(3000);
+module.exports = api;
+
+// app.use(api.handler('rest'));
+// app.use(express.static('public'));
+//
+// app.listen(3000);
